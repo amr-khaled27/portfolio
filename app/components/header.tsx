afe8c0b75c6fc7cc83dfeb014aef49c7d7996b74
amr-khaled27 <3moorkh2005@gmail.com>
2025-01-17 22:55:13 +0200

    first commit

---
+++ app/components/header.tsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <header className="p-2 flex justify-center fixed top-2 left-1/2 -translate-x-1/2 rounded-full bg-white/30 backdrop-blur-lg z-[999]">
+      <nav>
+        <ul className="flex gap-2">
+          <button className="btn btn-primary">Home</button>
+          <button className="btn btn-primary">Projects</button>
+          <button className="btn btn-primary">About</button>
+          <button className="btn btn-primary">Contact</button>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+ 
+export default Header;
