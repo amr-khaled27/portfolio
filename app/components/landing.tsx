3fc6120cd3293724fcab7d3dab794703e2dd60f4
amr-khaled27 <3moorkh2005@gmail.com>
2025-01-18 23:43:38 +0200

    added reveal animaton

--- app/components/landing.tsx
+++ app/components/landing.tsx
@@ -1,25 +1,29 @@
-import Shape from "./shape";
+import Reveal from "./reveal";
 
 const Landing = () => {
   return (
     <div className="bg-[#2b2b2b]">
-      <div className="h-screen relative p-4 overflow-hidden container mx-auto flex flex-col gap-4">
-        <div className="text text-white mt-36">
-          <p className="text-2xl">Hey There! 👋</p>
-          <div className="flex gap-2 items-end mb-4">
-            <span className="text-2xl">I&apos;m</span>
-            <h1 className="text-4xl font-bold">Amr Khaled.</h1>
-          </div>
-          <p className="text-sm mb-2">
-            A fullstack developer proficient in modern web technologies.
-          </p>
-          <p className="text-sm">
-            Let&apos;s build something amazing together!
-          </p>
-        </div>
-
-        <div className="flex justify-center mt-[150px]">
-          <Shape />
+      <div className="h-screen relative p-4 overflow-hidden container mx-auto flex flex-col justify-center gap-4">
+        <div className="text text-white flex flex-col *:w-fit">
+          <Reveal>
+            <p className="text-2xl">Hey There! 👋</p>
+          </Reveal>
+          <Reveal>
+            <div className="flex gap-2 items-end mb-4">
+              <span className="text-2xl">I&apos;m</span>
+              <h1 className="text-4xl font-bold">Amr Khaled.</h1>
+            </div>
+          </Reveal>
+          <Reveal>
+            <p className="text-sm mb-2">
+              A fullstack developer proficient in modern web technologies.
+            </p>
+          </Reveal>
+          <Reveal>
+            <p className="text-sm mb-2">
+              Let&apos;s build something amazing together!
+            </p>
+          </Reveal>
         </div>
       </div>
     </div>
