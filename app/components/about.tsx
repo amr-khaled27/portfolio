9b87cebc036942ffe31642983e3fc99739b9699b
amr-khaled27 <3moorkh2005@gmail.com>
2025-01-18 17:26:27 +0200

    rollback

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -1,17 +1,13 @@
 "use client";
-import { useEffect, useRef } from "react";
-import SplitType from "split-type";
+import { useRef } from "react";
+import "./about.css";
 
 const About = () => {
   const myText = useRef<HTMLHeadingElement | null>(null);
-  useEffect(() => {
-    const text = new SplitType(myText.current!);
-    console.log(text);
-  }, []);
   return ( 
     <div className="w-full h-screen bg-red-700 overflow-hidden flex justify-center">
       <div className="container mx-auto mt-16">
-        <h2 ref={myText} className="text-white text-3xl text-center">About The Developer</h2>
+        <h2 ref={myText} className="about text-white text-3xl text-center">About The Developer</h2>
       </div>
     </div>
    );
