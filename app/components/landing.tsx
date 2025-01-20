56dcec4cf776ed6ba34d990cf333158901b24e14
amr-khaled27 <3moorkh2005@gmail.com>
2025-01-20 16:58:29 +0200

    split into different component for better seo

--- app/components/landing.tsx
+++ app/components/landing.tsx
@@ -1,96 +1,19 @@
-"use client";
-import { useEffect, useRef } from "react";
 import "../styles/landing.css";
-import anime from "animejs";
-// import Reveal from "./reveal";
+import Tiles from "./tiles";
 
 const Landing = () => {
-  const parent = useRef<HTMLDivElement | null>(null);
-  const wrapper = useRef<HTMLDivElement | null>(null);
-
-  useEffect(() => {
-    let columns: number = 0;
-    let rows: number = 0;
-    let toggled: boolean = false;
-
-    const toggle = () => {
-      toggled = !toggled;
-      parent.current?.classList.toggle("toggled");
-    };
-
-    const handleOnClick = (index: number) => {
-      console.log(`clicked: ${index}`);
-      toggle();
-
-      anime({
-        targets: ".tile",
-        opacity: toggled ? 0 : 1,
-        delay: anime.stagger(50, {
-          grid: [columns, rows],
-          from: index,
-        }),
-      });
-    };
-
-    const createTile = (index: number) => {
-      const tile = document.createElement("div");
-
-      tile.classList.add("tile");
-
-      tile.style.backgroundColor = "#121212";
-
-      tile.onmouseover = () => {
-        tile.style.backgroundColor = "#323232";
-      };
-
-      tile.onmouseout = () => {
-        tile.style.backgroundColor = "#121212";
-      };
-
-      tile.style.opacity = toggled ? "0" : "1";
-
-      tile.onclick = () => handleOnClick(index);
-
-      return tile;
-    };
-
-    const createTiles = (quantity: number) => {
-      Array.from(Array(quantity)).map((tile, index) => {
-        wrapper.current?.appendChild(createTile(index));
-      });
-    };
-
-    const createGrid = () => {
-      wrapper.current!.innerHTML = "";
-
-      const size: number = (parent.current?.clientWidth || 0) > 800 ? 100 : 50;
-
-      columns = Math.floor((parent.current?.clientWidth || 0) / size);
-      rows = Math.floor((parent.current?.clientHeight || 0) / size);
-
-      wrapper.current?.style.setProperty("--columns", columns.toString());
-      wrapper.current?.style.setProperty("--rows", rows.toString());
-
-      createTiles(columns * rows);
-    };
-
-    createGrid();
-
-    window.onresize = () => createGrid();
-  }, []);
   return (
-    <div
-      ref={parent}
-      id="parent"
-      className="flex justify-center items-center h-screen"
-    >
-      <div ref={wrapper} id="tiles" className="tiles gap-1 p-1"></div>
-      <h1
+    <div>
+      <Tiles />
+      <div
         id="title"
         className="centered z-10 text-white text-6xl pointer-events-none"
       >
-        Hey There 👋
-      </h1>
+        Hey There 👋, <h1>I&apos;m Amr Khaled</h1>
+      </div>
+      <h2 id="subtitle" className="centered">
+        A fullstack developer proficient in modern web technologies.
+      </h2>
     </div>
   );
 };
---
+++ app/components/tiles.tsx
@@ -0,0 +1,91 @@
+"use client";
+import { useEffect, useRef } from "react";
+import "../styles/landing.css";
+import anime from "animejs";
+
+const Tiles = () => {
+  const parent = useRef<HTMLDivElement | null>(null);
+  const wrapper = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    let columns: number = 0;
+    let rows: number = 0;
+    let toggled: boolean = false;
+
+    const toggle = () => {
+      toggled = !toggled;
+      parent.current?.classList.toggle("toggled");
+    };
+
+    const handleOnClick = (index: number) => {
+      console.log(`clicked: ${index}`);
+      toggle();
+
+      anime({
+        targets: ".tile",
+        opacity: toggled ? 0 : 1,
+        delay: anime.stagger(50, {
+          grid: [columns, rows],
+          from: index,
+        }),
+      });
+    };
+
+    const createTile = (index: number) => {
+      const tile = document.createElement("div");
+
+      tile.classList.add("tile");
+
+      tile.style.backgroundColor = "#121212";
+
+      tile.onmouseover = () => {
+        tile.style.backgroundColor = "#323232";
+      };
+
+      tile.onmouseout = () => {
+        tile.style.backgroundColor = "#121212";
+      };
+
+      tile.style.opacity = toggled ? "0" : "1";
+
+      tile.onclick = () => handleOnClick(index);
+
+      return tile;
+    };
+
+    const createTiles = (quantity: number) => {
+      Array.from(Array(quantity)).map((tile, index) => {
+        wrapper.current?.appendChild(createTile(index));
+      });
+    };
+
+    const createGrid = () => {
+      wrapper.current!.innerHTML = "";
+
+      const size: number = (parent.current?.clientWidth || 0) > 800 ? 100 : 50;
+
+      columns = Math.floor((parent.current?.clientWidth || 0) / size);
+      rows = Math.floor((parent.current?.clientHeight || 0) / size);
+
+      wrapper.current?.style.setProperty("--columns", columns.toString());
+      wrapper.current?.style.setProperty("--rows", rows.toString());
+
+      createTiles(columns * rows);
+    };
+
+    createGrid();
+
+    window.onresize = () => createGrid();
+  }, []);
+  return (
+    <div
+      ref={parent}
+      id="parent"
+      className="flex justify-center items-center h-screen"
+    >
+      <div ref={wrapper} id="tiles" className="tiles gap-[1px] p-[1px]"></div>
+    </div>
+  );
+};
+
+export default Tiles;
