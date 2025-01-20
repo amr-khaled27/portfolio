943ea4b4cdf51f6e31a0834a259c1d1f95fa35c5
amr-khaled27 <3moorkh2005@gmail.com>
2025-01-20 23:46:44 +0200

    made header only show when you scroll up

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,6 +1,32 @@
+"use client";
+import { useState, useEffect, useRef, useCallback } from "react";
+
 const Header = () => {
+  const [showHeader, setShowHeader] = useState(true);
+  const lastScrollY = useRef<number>(window.scrollY);
+
+  const handleScroll = useCallback(() => {
+    if (window.scrollY < lastScrollY.current) {
+      setShowHeader(true);
+    } else {
+      setShowHeader(false);
+    }
+    lastScrollY.current = window.scrollY;
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [handleScroll]);
+
   return (
-    <header className="p-2 flex justify-center fixed top-2 left-1/2 -translate-x-1/2 rounded-full bg-white/30 backdrop-blur-lg z-[999]">
+    <header
+      className={`p-2 flex justify-center fixed top-2 left-1/2 -translate-x-1/2 rounded-full bg-white/30 backdrop-blur-lg z-[999] transition-transform duration-300 ${
+        showHeader ? "translate-y-0" : "-translate-y-[calc(100%_+_2rem)]"
+      }`}
+    >
       <nav>
         <ul className="flex gap-2">
           <button className="btn btn-primary">Home</button>
