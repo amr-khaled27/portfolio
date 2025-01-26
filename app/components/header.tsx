9ed9c4ea9491dfb10c8e01de658b5794e89e9c0e
amr-khaled27 <3moorkh2005@gmail.com>
2025-01-27 00:18:58 +0200

    Refactor Header component by removing unused state and effects

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,40 +1,7 @@
-"use client";
-import { useState, useEffect, useRef, useCallback } from "react";
-
 const Header = () => {
-  const [showHeader, setShowHeader] = useState(true);
-  useEffect(() => {
-    if (typeof window === "undefined") {
-      return;
-    }
-  }, []);
-
-  const lastScrollY = useRef<number>(window!.scrollY);
-
-  const handleScroll = useCallback(() => {
-    if (typeof window !== "undefined") {
-      if (window.scrollY < lastScrollY.current) {
-        setShowHeader(true);
-      } else {
-        setShowHeader(false);
-      }
-      lastScrollY.current = window.scrollY;
-    }
-  }, []);
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      window.addEventListener("scroll", handleScroll);
-      return () => {
-        window.removeEventListener("scroll", handleScroll);
-      };
-    }
-  }, [handleScroll]);
-
   return (
     <header
-      className={`p-2 flex justify-center fixed top-2 left-1/2 -translate-x-1/2 rounded-full bg-white/30 backdrop-blur-lg z-[999] transition-transform duration-300 ${
-        showHeader ? "translate-y-0" : "-translate-y-[calc(100%_+_2rem)]"
+      className={`p-2 flex justify-center fixed top-2 left-1/2 -translate-x-1/2 rounded-full bg-white/30 backdrop-blur-lg z-[999] transition-transform duration-300 "translate-y-0""
       }`}
     >
       <nav>
