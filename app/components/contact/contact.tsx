6ea41a7534b8222160e884b0c11833b75af497dc
amr-khaled27 <3moorkh2005@gmail.com>
2025-01-28 17:27:35 +0200

    Refactor Contact component form by removing Netlify attributes and input names

--- app/components/contact/contact.tsx
+++ app/components/contact/contact.tsx
@@ -28,18 +28,13 @@ const Contact = () => {
           </div>
         </div>
 
-        <form
-          method="POST"
-          data-netlify="true"
-          className="z-10 sm:flex sm:justify-center items-center text-white"
-        >
+        <form className="z-10 sm:flex sm:justify-center items-center text-white">
           <div className="flex flex-col gap-4">
             <Reveal style="w-full sm:w-[33vw]">
               <input
                 className="w-full sm:w-[33vw] bg-white/20 focus:outline-none backdrop-blur-sm p-4 rounded-xl"
                 type="text"
                 placeholder="Name"
-                name="name"
               />
             </Reveal>
             <Reveal style="w-full sm:w-[33vw]">
@@ -47,14 +42,12 @@ const Contact = () => {
                 className="w-full sm:w-[33vw] bg-white/20 focus:outline-none backdrop-blur-sm p-4 rounded-xl"
                 type="email"
                 placeholder="Email"
-                name="email"
               />
             </Reveal>
             <Reveal style="w-full sm:w-[33vw]">
               <textarea
                 className="w-full max-h-[200px] sm:w-[33vw] bg-white/20 focus:outline-none backdrop-blur-sm p-4 rounded-xl"
                 placeholder="Message"
-                name="message"
               ></textarea>
             </Reveal>
             <Reveal style="w-full">
