02766149eb5b9ed34ad493b09aa8f91c098be883
amr-khaled27 <3moorkh2005@gmail.com>
2025-02-01 11:19:54 +0200

    Add project type filtering and update projects data with type attribute

--- app/components/projects/projects.tsx
+++ app/components/projects/projects.tsx
@@ -15,10 +15,12 @@ export interface ProjectInterface {
     name: string;
     link: string;
   };
+  type: string;
 }
 
 const Projects = () => {
   const [projects, setProjects] = useState<ProjectInterface[]>([]);
+  const [type, setType] = useState("all");
 
   useEffect(() => {
     fetch("/projectsData.json")
@@ -31,19 +33,48 @@ const Projects = () => {
   return (
     <motion.div id="projects">
       <motion.div className="bg-colors-background p-4 flex flex-col justify-center text-white text-center">
-        <motion.div className="flex justify-center">
+        <motion.div className="flex flex-col justify-center">
           <p className="flex flex-col gap-1 pointer-events-none select-none text-4xl font-bold">
             Projects
-            <motion.span
-              transition={{ type: "spring", stiffness: 100, damping: 10 }}
-              className="h-1 bg-white rounded-full"
-            ></motion.span>
           </p>
+
+          <div>
+            <motion.div className="flex flex-col sm:flex-row gap-2 justify-center mt-4">
+              <motion.button
+                className={`${
+                  type === "all" ? "bg-colors-primary" : "bg-[#3f3f3f]"
+                } px-3 py-1 rounded-lg`}
+                onClick={() => setType("all")}
+              >
+                All
+              </motion.button>
+              <motion.button
+                className={`${
+                  type === "web" ? "bg-colors-primary" : "bg-[#3f3f3f]"
+                } px-3 py-1 rounded-lg`}
+                onClick={() => setType("web")}
+              >
+                Web Development
+              </motion.button>
+              <motion.button
+                className={`${
+                  type === "tools" ? "bg-colors-primary" : "bg-[#3f3f3f]"
+                } px-3 py-1 rounded-lg`}
+                onClick={() => setType("tools")}
+              >
+                Tools
+              </motion.button>
+            </motion.div>
+          </div>
         </motion.div>
 
         <motion.div className="mt-4 container mx-auto grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-2">
           {projects.map((project, i) => {
-            return <ProjectCard key={i} project={project} />;
+            if (type === "all") {
+              return <ProjectCard key={i} project={project} />;
+            } else if (project.type === type) {
+              return <ProjectCard key={i} project={project} />;
+            }
           })}
         </motion.div>
       </motion.div>
