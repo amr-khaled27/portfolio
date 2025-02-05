83526a67c20be07e29a5dba425a5b28d17e02fa9
amr-khaled27 <3moorkh2005@gmail.com>
2025-02-05 18:46:57 +0200

    Refactor header component styles to remove redundant background classes, and improve performance

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -9,32 +9,32 @@ const Header = () => {
       }}
       initial="hidden"
       animate="visible"
-      className={`p-2 flex justify-center fixed top-2 left-1/2 -translate-x-1/2 rounded-full bg-white/30 backdrop-blur-lg z-[999] transition-transform duration-300 "translate-y-0""
+      className={`p-2 flex justify-center fixed top-2 left-1/2 -translate-x-1/2 rounded-full bg-white/30 z-[999] transition-transform duration-300 "translate-y-0""
       }`}
     >
       <nav>
         <ul className="flex gap-2">
           <a
             href="#landing"
-            className="py-2 px-3 sm:px-5 rounded-full bg-white/50 hover:scale-95 duration-300 hover:bg-white/75 text-black font-thin"
+            className="py-2 px-3 sm:px-5 rounded-full bg-white hover:scale-95 duration-300 hover:bg-white/75 text-black font-thin"
           >
             Home
           </a>
           <a
             href="#about"
-            className="py-2 px-3 sm:px-5 rounded-full bg-white/50 hover:scale-95 duration-300 hover:bg-white/75 text-black font-thin"
+            className="py-2 px-3 sm:px-5 rounded-full bg-white hover:scale-95 duration-300 hover:bg-white/75 text-black font-thin"
           >
             About
           </a>
           <a
             href="#projects"
-            className="py-2 px-3 sm:px-5 rounded-full bg-white/50 hover:scale-95 duration-300 hover:bg-white/75 text-black font-thin"
+            className="py-2 px-3 sm:px-5 rounded-full bg-white hover:scale-95 duration-300 hover:bg-white/75 text-black font-thin"
           >
             Projects
           </a>
           <a
             href="#contact"
-            className="py-2 px-3 sm:px-5 rounded-full bg-white/50 hover:scale-95 duration-300 hover:bg-white/75 text-black font-thin"
+            className="py-2 px-3 sm:px-5 rounded-full bg-white hover:scale-95 duration-300 hover:bg-white/75 text-black font-thin"
           >
             Contact
           </a>
